fec69b2474340d34ef42b8e798dbba351566b512
Faisal Fehad <ifaisal73it@gmail.com>
2015-08-05 19:47:23 +0000

    fix devise bug, add footer, cass changes and add user name field

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150730194324) do
+ActiveRecord::Schema.define(version: 20150805135611) do
 
   create_table "pins", force: :cascade do |t|
     t.text     "description"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 20150730194324) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
