39f6baad031c1e5f7c665f5113d1e68675006b18
FaisalFehad <ifaisal73it@gmail.com>
2015-08-08 09:25:42 +0000

    remove 'picture' field

--- db/migrate/20150728191632_create_pins.rb
+++ db/migrate/20150728191632_create_pins.rb
@@ -2,7 +2,7 @@ class CreatePins < ActiveRecord::Migration
   def change
     create_table :pins do |t|
       t.text :description
-      t.string :picture
+      
 
       t.timestamps null: false
     end
--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150805135611) do
+ActiveRecord::Schema.define(version: 20150807200742) do
 
   create_table "pins", force: :cascade do |t|
     t.text     "description"
-    t.string   "picture"
     t.datetime "created_at",         null: false
     t.datetime "updated_at",         null: false
     t.string   "image_file_name"
@@ -41,9 +40,11 @@ ActiveRecord::Schema.define(version: 20150805135611) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.string   "name"
+    t.string   "username"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["username"], name: "index_users_on_username", unique: true
 
 end
