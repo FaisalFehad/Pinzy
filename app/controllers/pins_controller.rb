08abeb08d991b12b9064b6f288c93edbfb9940d6
FaisalFehad <ifaisal73it@gmail.com>
2015-08-09 21:04:57 +0000

    change the like button

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,5 +1,5 @@
 class PinsController < ApplicationController
-  before_action :set_pin, only: [:show, :edit, :update, :destroy]
+  before_action :set_pin, only: [:show, :edit, :update, :destroy, :upvote]
   before_action :authenticate_user!, except: [:index, :show]
 
   # GET /pins
