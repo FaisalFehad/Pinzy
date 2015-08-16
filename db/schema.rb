447087ada5d03a661903dca46e6decf4f91ce463
FaisalFehad <faisalfehad@gmail.com>
2015-08-16 17:15:00 +0000

    remove comments scaffold and add model and controller, add 'thin'

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150809183453) do
+ActiveRecord::Schema.define(version: 20150816100133) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string   "comment"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "pins", force: :cascade do |t|
     t.text     "description"
